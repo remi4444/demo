66cb882b7a5d0d2993ad5e5feaea6f09311d0b2f
puvilland <remi.puvilland@gmail.com>
2020-01-20 04:54:18 -0800

    First commit

---
+++ src/test/java/Ebay.java
@@ -0,0 +1,81 @@
+
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Ebay {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        String s = System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        baseUrl = "https://www.google.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testEbay() throws Exception {
+        driver.get("https://www.ebay.fr/");
+        driver.findElement(By.linkText("Marques")).click();
+        driver.findElement(By.linkText("ACIS")).click();
+        driver.findElement(By.xpath("//li[@id='w7-items[0]']/div/div[2]/a/h3")).click();
+        try {
+            assertEquals("Classique", driver.findElement(By.xpath("//*[@id=\"viTabs_0_is\"]/div/table/tbody/tr[2]/td[4]/span")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
